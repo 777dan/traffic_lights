d8f49c8aa1ef96fe313c69714af2369872759f98
Danoslo 255 <68803448+777dan@users.noreply.github.com>
2021-11-18 19:24:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -99,28 +99,56 @@
 function initCounter(name) {
     let count = 0;
     let color;
-    let colorList = ['red', 'yellow', 'green', 'yellow'];
+    let colorList = ['red', 'yellow', 'green'];
     let innerCounter = function () {
+        // let allowR = false;
+        // let allowG = false;
         color = colorList[count];
+        // allowR = true;
         count++;
-        if (count == 4) {
+        if (count == 3) {
             count = 0;
         }
+        // if (count == 0) {
+        //     allowR = true;
+        // }
+        // else if (count == 2) {
+        //     allowG = true;
+        // }
+        // else if (count == 3) {
+        //     count = 2;
+        //     allowR = false;
+        //     allowG = false;
+        // }
+        // else if (count == 2 && allowR == false) {
+        //     count = 0;
+        //     allowR = true;
+        // }
+        // else if (count == 2 && allowG == false) {
+        //     count = 1;
+        //     allowG = true;
+        // }
+        // // else {
+        // //     count = 1;
+        // // }
+        // count++;
+        // color = colorList[count];
         return name + " " + color;
     };
     return innerCounter;
 }
 
 let counter1 = initCounter("first");
+let counter2 = initCounter("second");
 console.log(counter1());
+console.log(counter2());
 console.log(counter1());
-console.log(counter1());
-console.log(counter1());
+console.log(counter2());
 console.log(counter1());
 console.log(counter1());
 
-let counter2 = initCounter("second");
+// let counter2 = initCounter("second");
 console.log(counter2());
+console.log(counter1());
 console.log(counter2());
-console.log(counter2());
-console.log(counter2());
+console.log(counter1());
